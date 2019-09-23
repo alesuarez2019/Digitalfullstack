06e3cf15572bbecd5ffc6bc66765397fce78b9d0
Alejandro <alesuarezfc@gmail.com>
2019-09-23 20:06:43 -0300

    validacion parcial de carrito

--- proyectointegrador/funciones.php
+++ proyectointegrador/funciones.php
@@ -1,19 +1,13 @@
 <?php
 function guardar($datos) {
 $file = 'usuarios.txt';
-    
+
     foreach ($datos as $dato) {
         $person = $dato . ";";
         file_put_contents($file, $person, FILE_APPEND | LOCK_EX);
-        
+
     }
     $newline = "\n";
     file_put_contents($file, $newline, FILE_APPEND | LOCK_EX);
-        
-    
-
 }
-
 ?>
-
-
